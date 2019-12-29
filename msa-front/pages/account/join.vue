08184d462b051261917f9bcf89cecb2191a8f640
hyeokdev <hyeokdev@gmail.com>
2019-12-29 18:14:02 +0900

    2회차 커밋

---
+++ msa-front/pages/account/join.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="wrap_join">
+      <p class="join_text">Join</p>
+      <input type="text" placeholder="Insert ID" class="join_inp">
+      <input type="password" placeholder="Insert Password" class="join_inp">
+      <input type="password" placeholder="Insert Password Check" class="join_inp">
+      <button class="join_btn">JOIN</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #wrap_join {
+        position: absolute;
+        top: 20%;
+        left: 40%;
+        border: 2px solid lightgray;
+        border-radius: 5px;
+        padding: 2em 2em 2em 2em;
+        text-align: center;
+    }
+    .join_text {
+        font-size: 24px;
+        font-weight: 600;
+        color: dodgerblue;
+        margin-bottom: 40px
+    }
+    .join_inp {
+        display: block;
+        margin-bottom: 10px;
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        width: 100%;
+        height: 30px;
+    }
+    .join_btn {
+        width: 100%;
+        background-color: dodgerblue;
+        color: white;
+        border: 1px solid dodgerblue;
+        border-radius: 5px;
+        height: 30px;
+    }
+</style>
---
+++ msa-front/pages/account/login.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="wrap_login">
+        <div class="border_login">
+            <p class="login_text">Login</p>
+            <div>
+                <input type="text" placeholder="Insert ID" class="login_inp">
+            </div>
+            <div>
+                <input type="password" placeholder="Insert Password" class="login_inp">
+            </div>
+            <button class="login_btn">LOGIN</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #wrap_login {
+        width: 100%;
+        height: 100%;
+    }
+    .border_login {
+        position: absolute;
+        text-align: center;
+        left: 40%;
+        top: 30%;
+        width: 300px;
+        border: 2px solid lightgray;
+        border-radius: 5px;
+    }
+    .login_text {
+        font-size: 2rem;
+        font-weight: 600;
+        color: dodgerblue;
+        margin-bottom: 50px;
+    }
+    .login_inp {
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        width: 80%;
+        height: 30px;
+        margin-bottom: 10px;
+    }
+    .login_btn {
+        cursor: pointer;
+        border: 1px solid dodgerblue;
+        background-color: dodgerblue;
+        color: white;
+        border-radius: 5px;
+        width: 80%;
+        height: 30px;
+        margin-bottom: 10px;
+    }
+</style>
